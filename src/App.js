7f04aadac1883f8272d2bcc075417d58a0f351bc
Nivaldo Cavalcanti <nivalduh@gmail.com>
2020-12-20 16:00:51 -0300

    Refactor: changing the code to display at the mirror section the text typed into the input field

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    query: ''
+  }
+  updateQuery = (query) => {
+    this.setState(() => ({
+      query: query.trim()
+    }))
+  }
   render() {
+    const { query } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -11,9 +20,10 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
+          <input type="text" placeholder="Say Something" value={query} onChange={(event) => this.updateQuery(event.target.value)}
+           />
           <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
+          <p>{query}</p>
         </div>
       </div>
     );
